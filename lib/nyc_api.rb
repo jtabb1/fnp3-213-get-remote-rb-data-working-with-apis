a23bbc157fca5c522b0268806ef25b82c2cd7e06
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-07-27 12:04:54 -0400

    Complete code-along.

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -1,3 +1,5 @@
+# Code-along complete!
+
 require 'net/http'
 require 'open-uri'
 require 'json'
@@ -12,7 +14,17 @@ class GetPrograms
     response.body
   end
 
+  def program_school
+  # we use the JSON library to parse the API response into nicely formatted JSON
+    programs = JSON.parse(self.get_programs)
+    programs.collect do |program|
+      program["agency"]  
+    end
+  end    
+
 end
 
-programs = GetPrograms.new.get_programs
-puts programs
+# programs = GetPrograms.new.get_programs
+# puts programs
+programs = GetPrograms.new
+puts programs.program_school.uniq
